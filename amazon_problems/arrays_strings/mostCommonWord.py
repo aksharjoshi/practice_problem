054dfdd664f96e30fd2fe90c7755c5ed26f5e020
Akshar Joshi <akshar.joshi@fireeye.com>
2020-06-25 16:37:45 -0700

    mnostcommon words approach using counter

--- amazon_problems/arrays_strings/mostCommonWord.py
+++ amazon_problems/arrays_strings/mostCommonWord.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import Counter
 
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
@@ -9,20 +9,21 @@ class Solution(object):
         :rtype: str
         """
         words = re.sub(r"[^a-zA-Z0-9]+", ' ', paragraph).strip()
-        words = words.split(' ')
+        words = words.lower().split(' ')
         most_common_dict = {}
         
-        for word in words:
-            word = word.lower().strip()
-            if word not in banned and word:
-                if word in most_common_dict:
-                    most_common_dict[word] = most_common_dict[word] + 1
-                else:
-                    most_common_dict[word] = 1
+        # for word in words:
+        #     word = word.lower().strip()
+        #     if word not in banned and word:
+        #         if word in most_common_dict:
+        #             most_common_dict[word] = most_common_dict[word] + 1
+        #         else:
+        #             most_common_dict[word] = 1
         
-        most_common_dict = sorted(most_common_dict.items(), key=lambda x: x[1], reverse=True)
+        # most_common_dict = sorted(most_common_dict.items(), key=lambda x: x[1], reverse=True)
+        return Counter(word for word in words if word not in banned).most_common(1)[0][0]
         #print most_common_dict
-        return next(iter(most_common_dict))[0]
+        #return next(iter(most_common_dict))[0]
 
         
         
